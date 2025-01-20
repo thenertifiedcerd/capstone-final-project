aa5d49a39d4585a0dffa3f0c0ad758cf185eedd4
thenertifiedcerd <benzenefemiakinwande@gmail.com>
2025-01-20 17:53:34 -0500

    Components added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
+import Landingpage from './Components/Landing_Page/Landingpage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+          <Landingpage />
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            
+            {/* Define individual Route components for different pages */}
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
